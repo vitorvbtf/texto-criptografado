5d7c3a4d8db14901734cd2a91d74ee17519fd874
vitorvbtf <vitorvbtf@gmail.com>
2024-01-30 09:06:22 -0300

    realizando algumas alterções básicas

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ let p = document.querySelector('p.nada-encontrado')
 
 
 function criptografar(){
+    mensagemCriptografada = ""
     textoDoUsuario = document.getElementById
     ('caixa-de-texto').value
 
-    resposta.innerHTML = ""
     for (let i = 0; i < textoDoUsuario.length; i++) {
         switch (textoDoUsuario[i]) {
             case "a":
@@ -37,7 +37,7 @@ function criptografar(){
     imagem.setAttribute("hidden", true)
     h1.setAttribute("hidden", true)
     p.setAttribute("hidden", true)
-    return resposta.innerHTML = mensagemCriptografada
+    return resposta.textContent = mensagemCriptografada
     } else{
         alert('campo vazio!')
     }
@@ -64,27 +64,14 @@ function descriptografar(){
 }
 
 function copiar() {
-    // Selecionar o elemento de resposta
     const elementoResposta = document.getElementById('resposta');
 
-    // Criar uma área de transferência
     const areaTransferencia = document.createElement('textarea');
-    
-    // Definir o valor da área de transferência como o texto da resposta
     areaTransferencia.value = elementoResposta.innerText;
-
-    // Adicionar a área de transferência ao corpo do documento
     document.body.appendChild(areaTransferencia);
-
-    // Selecionar o texto na área de transferência
     areaTransferencia.select();
-    
-    // Copiar o texto
     document.execCommand('copy');
-
-    // Remover a área de transferência após a cópia
     document.body.removeChild(areaTransferencia);
 
-    // Exibir uma mensagem de sucesso
     alert('Texto copiado com sucesso!');
 }
